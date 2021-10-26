37bba1abecebbb6c34a8bf4a53395f985d81984c
duongnpGinnov <duongnp@ginnov.tech>
2021-10-26 17:15:09 +0700

    update code

--- src/settings.js
+++ src/settings.js
@@ -2,7 +2,7 @@ import { createClient, createMicrophoneAndCameraTracks } from "agora-rtc-react";
 
 const appId = "d67702adcbfc43cfbf51b85e192d2f48";
 const token =
-  "006d67702adcbfc43cfbf51b85e192d2f48IABA14b37pwlIq72Hu8C5qPBCwNFRHknhl2F3AS/Kt5cxe3Q8zEAAAAAEADJD5AXObN4YQEAAQA5s3hh";
+  "006d67702adcbfc43cfbf51b85e192d2f48IACgOx6hxWp/Gz8mkeieDbX/YW7r2nPhqIFZ/sBETrsbSu3Q8zEAAAAAEADJD5AXEyZ5YQEAAQAUJnlh";
 
 export const config = { mode: "rtc", codec: "vp8", appId: appId, token: token };
 export const useClient = createClient(config);
